1015aa04825306708d18ba4cbfabdf8a7583c4e4
Anirudh Joshi <anirudh.53.aj@gmail.com>
2024-11-23 01:48:31 +0530

    sucess

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { AppBar, Button, Tab, Tabs, Toolbar, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import AddModeratorIcon from '@mui/icons-material/AddModerator';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+
+    const navigate = useNavigate()
+    const [value , setValue] = useState()
+
+    const nav = ()=>{
+        navigate("/")
+    }
+
   return (
-    <div>Dashboard</div>
+    <>
+    <AppBar sx={{background: "#063970"}}>
+        <Toolbar>
+            <AddModeratorIcon/>
+            <Tabs textColor='inherit' value={value} onChange={(e,value)=> setValue(value)} indicatorColor='secondary'>
+                <Tab label='Home'/>
+                <Tab label='Services'/>
+                <Tab label='About'/>
+                <Tab label='Setting'/>
+            </Tabs>
+
+            <Button onClick={nav} sx={{ml:'auto'}} variant='contained'>Log out</Button>
+        </Toolbar>
+    </AppBar>
+    </>
   )
 }
 
